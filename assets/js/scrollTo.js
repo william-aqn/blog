f2b41b8e40953b82625afd680014983b7f7793a6
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2023-06-15 17:17:07 +0300

    Update scrollTo.js

--- assets/js/scrollTo.js
+++ assets/js/scrollTo.js
@@ -2,33 +2,33 @@ $(window).load(function () {
     $(document).on("scroll", onScroll);
 
     //smoothscroll
-    // $('.scroll-icon').on('click', function (e) {
-    //     e.preventDefault();
-    //     $(this).addClass('active');
+    $('.scroll-icon').on('click', function (e) {
+        e.preventDefault();
+        $(this).addClass('active');
 
-    //     var target = this.hash,
-    //         menu = target;
-    //     $target = $(target);
-    //     $('html, body').stop().animate({
-    //         'scrollTop': $target.offset().top + 2
-    //     }, 1000, 'swing', function () {
-    //         window.location.hash = target;
-    //     });
-    // });
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 
-    // $('.sf-menu').find('a[href^="#"]').on('click', function (e) {
-    //     e.preventDefault();
-    //     $(this).addClass('active');
+    $('.sf-menu').find('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(this).addClass('active');
 
-    //     var target = this.hash,
-    //         menu = target;
-    //     $target = $(target);
-    //     $('html, body').stop().animate({
-    //         'scrollTop': $target.offset().top + 2
-    //     }, 1000, 'swing', function () {
-    //         window.location.hash = target;
-    //     });
-    // });
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
 
 
@@ -43,7 +43,7 @@ function onScroll(event) {
         return;
     }
 
-    $('.sf-menu').find('a').each(function () {
+    $('.sf-menu').find('a[href^="#"]').each(function () {
         var currLink = $(this);
         try {
             var refElement = $(currLink.attr("href"));
