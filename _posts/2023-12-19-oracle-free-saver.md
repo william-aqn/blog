cbc02207bd0360e5afa85091a96708805e99bdd9
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2023-12-19 22:23:38 +0300

    Создаём активность на серверах Oracle free tier

---
+++ _posts/2023-12-19-oracle-free-saver.md
@@ -0,0 +1,27 @@
+---
+layout: post
+title: "Создаём активность на серверах Oracle free tier"
+description: "Защищаем инстанс от отключения"
+tags: nodejs cpu
+---
+# Создаём активность на серверах Oracle free tier
+
+Надо как то защитить сервер на Oracle, что бы не инстанс не отключали из за новой политики 
+[Oracle Reclamation of Idle Compute Instances](https://docs.oracle.com/en-us/iaas/Content/FreeTier/freetier_topic-Always_Free_Resources.htm)
+
+```
+Idle Always Free compute instances may be reclaimed by Oracle. Oracle will deem virtual machine and bare metal compute instances as idle if, during a 7-day period, the following are true:
+
+    CPU utilization for the 95th percentile is less than 20%
+    Network utilization is less than 20%
+    Memory utilization is less than 20% (applies to A1 shapes only)
+```
+
+Хотят нагрузку на cpu - делаем: [cpu.js](/assets/blog/oracle/cpu.js)
+```js
+import Cpu from "./cpu.js";
+new Cpu(20);
+```
+![htop](/assets/blog/oracle/oracle-node.png)
+
+
---
+++ assets/blog/oracle/cpu.js
@@ -0,0 +1,61 @@
+/**
+ * Чат ЖПТ представляет
+ */
+class Cpu {
+    /**
+     * Создание нагрузки на CPU
+     * @param {*} targetLoad 
+     */
+    constructor(targetLoad) {
+        this.loadCPU(targetLoad)
+    }
+
+    /**
+     * Простое вычисление для создания нагрузки
+     * @param {*} duration 
+     */
+    busyWork(duration) {
+        const end = Date.now() + duration;
+        while (Date.now() < end) {
+            // 
+            Math.sqrt(Math.random());
+        }
+    }
+
+    /**
+     * Спим
+     * @param {*} duration 
+     * @returns 
+     */
+    sleep(duration) {
+        return new Promise(resolve => setTimeout(resolve, duration));
+    }
+
+    /**
+     * Главная функция для создания нагрузки на CPU
+     * @param {*} targetLoad 
+     */
+    async loadCPU(targetLoad) {
+        const loadDuration = 100; // Время в мс, которое мы занимаем CPU
+        const totalDuration = (loadDuration / targetLoad) * 100; // Общее время цикла, чтобы достичь целевой нагрузки
+        const sleepDuration = totalDuration - loadDuration; // Время в мс, на которое мы "отдыхаем"
+        // console.log("totalDuration", totalDuration, "sleepDuration", sleepDuration);
+        while (true) {
+            const startTime = Date.now();
+
+            // Занимаем CPU
+            this.busyWork(loadDuration);
+
+            // Вычисляем, сколько времени прошло и сколько нужно 'отдохнуть'
+            const workTime = Date.now() - startTime;
+            const remainingSleep = sleepDuration - workTime;
+
+            if (remainingSleep > 0) {
+                // Отдыхаем, если осталось время
+                await this.sleep(remainingSleep);
+            }
+        }
+    }
+}
+
+export default Cpu;
